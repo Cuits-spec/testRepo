0913c81a8bfaa25e17054357311876d300a83d24
哈哈 <jianwei.ke@versa-ai.com>
2021-05-06 18:53:17 +0800

    字符串练习题

---
+++ str_method.py
@@ -0,0 +1,19 @@
+# 将字符串'adcd'转成大写
+str1 = 'adcd'
+print(str1.upper())
+
+# 计算字符串'cd'在字符串'adcd'中出现的位置
+print(str1.find('cd'))
+
+# 用，号分割字符串'a,b,c,d'
+str2 = 'a,b,c,d'
+print(str2.split(','))
+
+# str3 = 'Python is good' 将Python替换成python
+str3 = 'Python is good'
+print(str3.lower())
+
+# str4 = 'python修炼第一期.html',写程序从这个字符串中获得.html前面部分，用尽可能多的方法
+str4 = 'python修炼第一期.html'
+print(str4.replace('.html', ' '))
+print(str4.split('.html'))
