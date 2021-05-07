5a74a69d8850836fe1bae248945cc800ac260374
哈哈 <jianwei.ke@versa-ai.com>
2021-05-07 19:53:12 +0800

    list_method

---
+++ list_method.py
@@ -0,0 +1,23 @@
+list1 = [1,2,3,4,5,6]
+# 求列表长度
+print(len(list1))
+# 判断6是否在列表中
+print(6 in list1)
+# list1 + [7,8,9]结果是什么
+list2 = list1 + [7,8,9]
+print(list2)
+# list1 * 2结果
+list3 = list1 * 2
+print(list3)
+# 求列表元素最大值
+print(max(list1))
+# 求列表元素最小值
+print(min(list1))
+# 列表所有元素和
+print(sum(list1))
+# 在索引1后面新增一个元素10
+list1.insert(1, 10)
+print(list1)
+# 在列表末尾新增一个元素20
+list1.append(20)
+print(list1)
