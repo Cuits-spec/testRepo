97b97236d38d3c6dcdb7e7b40f00163168ce0f8f
哈哈 <jianwei.ke@versa-ai.com>
2021-05-07 20:58:22 +0800

    加了一些

--- list_method.py
+++ list_method.py
@@ -21,3 +21,35 @@ print(list1)
 # 在列表末尾新增一个元素20
 list1.append(20)
 print(list1)
+# list2 = [1,[4,6],True] 将列表里数字修改成原来的2倍
+list2 = [1,[4,6],True]
+list2[0] = 2
+list2[1][0] = 8
+list2[1][1] = 12
+print(list2)
+
+list3 = [123]
+list4 = [456]
+list3.extend(list4)
+print(list3)
+
+str1 = '123'
+str2 = '456'
+str3 = str1 + str2
+print(str3)
+
+list5 = [2,5,6,7,8,9,2,9,9]
+# 找出列表最大值
+print(max(list5))
+# 找出列表最小值
+print(min(list5))
+# 找出列表最大值个数
+print(list5.count(max(list5)))
+# 计算列表所有元素和
+print(sum(list5))
+# 计算列表元素平均值
+print(sum(list5)/len(list5))
+# 计算列表长度
+print(len(list5))
+# 找出元素6在列表的索引
+print(list5.index(6))
