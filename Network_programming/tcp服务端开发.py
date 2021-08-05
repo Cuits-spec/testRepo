46979ad75868cf02a6d1861c5818aea544b28c7c
Cuits-spec <83103869+Cuits-spec@users.noreply.github.com>
2021-08-05 17:32:40 +0800

    Update tcp服务端开发.py
    补全tcp服务端

--- Network_programming/tcp服务端开发.py
+++ Network_programming/tcp服务端开发.py
@@ -1,24 +1,36 @@
 import socket
 
-
 if __name__ == '__main__':
-    # 创建tcp客户端套接字
-    # 1. AF_INET：表示ipv4
-    # 2. SOCK_STREAM: tcp传输协议
-    tcp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # 和服务端应用程序建立连接
-    tcp_client_socket.connect(("192.168.131.62", 8080))
-    # 代码执行到此，说明连接建立成功
-    # 准备发送的数据
-    send_data = "你好服务端，我是客户端小黑!".encode("gbk")
-    # 发送数据
-    tcp_client_socket.send(send_data)
-    # 接收数据, 这次接收的数据最大字节数是1024
-    recv_data = tcp_client_socket.recv(1024)
-    # 返回的直接是服务端程序发送的二进制数据
-    print(recv_data)
-    # 对数据进行解码
+    # 创建tcp服务端套接字
+    tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 设置端口号复用，让程序退出端口号立即释放
+    tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+    # 给程序绑定端口号
+    tcp_server_socket.bind(("", 8989))
+    # 设置监听
+    # 128:最大等待建立连接的个数， 提示： 目前是单任务的服务端，同一时刻只能服务与一个客户端，后续使用多任务能够让服务端同时服务与多个客户端，
+    # 不需要让客户端进行等待建立连接
+    # listen后的这个套接字只负责接收客户端连接请求，不能收发消息，收发消息使用返回的这个新套接字来完成
+    tcp_server_socket.listen(128)
+    # 等待客户端建立连接的请求, 只有客户端和服务端建立连接成功代码才会解阻塞，代码才能继续往下执行
+    # 1. 专门和客户端通信的套接字： service_client_socket
+    # 2. 客户端的ip地址和端口号： ip_port
+    service_client_socket, ip_port = tcp_server_socket.accept()
+    # 代码执行到此说明连接建立成功
+    print("客户端的ip地址和端口号:", ip_port)
+    # 接收客户端发送的数据, 这次接收数据的最大字节数是1024
+    recv_data = service_client_socket.recv(1024)
+    # 获取数据的长度
+    recv_data_length = len(recv_data)
+    print("接收数据的长度为:", recv_data_length)
+    # 对二进制数据进行解码
     recv_content = recv_data.decode("gbk")
-    print("接收服务端的数据为:", recv_content)
-    # 关闭套接字
-    tcp_client_socket.close()
+    print("接收客户端的数据为:", recv_content)
+    # 准备发送的数据
+    send_data = "ok, 问题正在处理中...".encode("gbk")
+    # 发送数据给客户端
+    service_client_socket.send(send_data)
+    # 关闭服务与客户端的套接字， 终止和客户端通信的服务
+    service_client_socket.close()
+    # 关闭服务端的套接字, 终止和客户端提供建立连接请求的服务
+    tcp_server_socket.close()
